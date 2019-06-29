83591080385dd5557d215eb3ca46cc312cb7542c
masappe <masato6820@gmail.com>
2019-06-29 20:36:37 +0900

    Initial Commit

---
+++ Yacht_ScoreCalculation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yacht_ScoreCalculation
+//
+//  Created by Masato Hayakawa on 2019/06/29.
+//  Copyright © 2019 masappe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
